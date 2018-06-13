7d2bcaca797f1564e98af95cf437a82208e9733a
(Amanda Orbe) <garayomiriam@gmail.com>
2018-06-13 17:32:55 +0200

    nosotros is no longer a taxon but a set of pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+
+  get 'nosotros/nosotros'
+  get 'nosotros/historia'
+  get 'nosotros/prensa'
+  get 'nosotros/pasarela'
+  get 'nosotros/tiendas'
+  get 'pages/nosotros'
+  get 'pages/historia'
+  get 'pages/prensa'
+  get 'pages/pasarela'
+  get 'pages/tiendas'
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
